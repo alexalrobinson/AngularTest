cae5b26cfb70c06819b8ea36ea6262f8fd40847a
Alexa Robinson <ARobinson@frontierssi.com>
2021-11-07 16:12:41 -0500

    fix check word

--- app.js
+++ app.js
@@ -5,31 +5,31 @@ angular
   .controller('balancedBracesController', [
     '$scope',
     $scope => {
-			// input binding
+      // input binding
       $scope.newPairName = ''
       $scope.newOpenChar = ''
       $scope.newClosingChar = ''
       $scope.wordToCheck = ''
-			
-			// other state handlers
+
+      // other state handlers
       $scope.pairsList = [
         { name: 'Brackets', chars: ['{', '}'] },
         { name: 'Square Brackets', chars: ['[', ']'] },
         { name: 'Paranthesis', chars: ['(', ')'] }
       ]
 
-			$scope.pairsLookUp = {
-				open: {
-					'{': '}',
-					'[': ']',
-					'(': ')'
-				}, 
-				close: {
-					'}': '{', 
-					']': '[', 
-					')': '('
-				}
-			}
+      $scope.pairsLookUp = {
+        open: {
+          '{': '}',
+          '[': ']',
+          '(': ')'
+        },
+        close: {
+          '}': '{',
+          ']': '[',
+          ')': '('
+        }
+      }
 
       $scope.checkedWordsHistory = []
 
@@ -38,41 +38,40 @@ angular
       // functions
       $scope.addNewPair = () => {
         const {
-					pairsLookUp,
+          pairsLookUp,
           newPairName: name,
           newOpenChar: open,
           newClosingChar: close
         } = $scope
 
         $scope.pairsList.push({ name, chars: [open, close] })
-				$scope.pairsLookUp = {...pairsLookUp, newOpenChar: newClosingChar}
-				$scope.addPair = false
+        $scope.pairsLookUp = { ...pairsLookUp, newOpenChar: newClosingChar }
+        $scope.addPair = false
       }
 
-      $scope.checkWord() = () => {
-				const word = $scope.wordToCheck
+      $scope.checkWord = () => {
+        const word = $scope.wordToCheck
 
-				if (word === '') return
+        if (word === '') return
 
-				const openingChars = []
+        const openingChars = []
 
-				for(const char of word) {
-					if (char in $scope.pairsLookUp.open) {
-						openingChars.push(char)
-					}
-					else if (char in $scope.pairsLookUp.close) {
-						const lastOpenChar = openingChars.pop()
+        for (const char of word) {
+          if (char in $scope.pairsLookUp.open) {
+            openingChars.push(char)
+          } else if (char in $scope.pairsLookUp.close) {
+            const lastOpenChar = openingChars.pop()
 
-						// word is not balanced
-						if (char !== $scope.pairsLookUp.open[lastOpenChar]) {
-							$scope.checkedWordsHistory.unshift({word, balanced: false})
-						}
-					}
-				}
+            // word is not balanced
+            if (char !== $scope.pairsLookUp.open[lastOpenChar]) {
+              $scope.checkedWordsHistory.unshift({ word, balanced: false })
+            }
+          }
+        }
 
-				// word is balanced
-				$scope.checkedWordsHistory.unshift({word, balanced: true})
-			}
+        // word is balanced
+        $scope.checkedWordsHistory.unshift({ word, balanced: true })
+      }
     }
   ])
 
@@ -82,15 +81,15 @@ angular
     }
   })
 
-	.directive('ngEnter', () => {
+  .directive('ngEnter', () => {
     return (scope, element, attrs) => {
-        element.bind("keydown keypress", (event) => {
-            if (event.which === 13) {
-                scope.$apply(() => {
-                    scope.$eval(attrs.ngEnter)
-                })
-                event.preventDefault()
-            }
-        })
+      element.bind('keydown keypress', event => {
+        if (event.which === 13) {
+          scope.$apply(() => {
+            scope.$eval(attrs.ngEnter)
+          })
+          event.preventDefault()
+        }
+      })
     }
-	})
+  })
