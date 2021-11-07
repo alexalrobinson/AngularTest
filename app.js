6bb6c0248f1edfc6d93a68b8c4056ec777c4eec0
Alexa Robinson <ARobinson@frontierssi.com>
2021-11-07 16:06:01 -0500

    implement functionality

--- app.js
+++ app.js
@@ -1,20 +1,96 @@
 //this is defining the app module and the balancedBracesTest directive within that module
 angular
   .module('app', [])
+
   .controller('balancedBracesController', [
     '$scope',
     $scope => {
+			// input binding
+      $scope.newPairName = ''
+      $scope.newOpenChar = ''
+      $scope.newClosingChar = ''
+      $scope.wordToCheck = ''
+			
+			// other state handlers
       $scope.pairsList = [
         { name: 'Brackets', chars: ['{', '}'] },
         { name: 'Square Brackets', chars: ['[', ']'] },
         { name: 'Paranthesis', chars: ['(', ')'] }
       ]
 
-      $scope.checkedWords = []
+			$scope.pairsLookUp = {
+				open: {
+					'{': '}',
+					'[': ']',
+					'(': ')'
+				}, 
+				close: {
+					'}': '{', 
+					']': '[', 
+					')': '('
+				}
+			}
+
+      $scope.checkedWordsHistory = []
+
+      $scope.addPair = false
+
+      // functions
+      $scope.addNewPair = () => {
+        const {
+					pairsLookUp,
+          newPairName: name,
+          newOpenChar: open,
+          newClosingChar: close
+        } = $scope
+
+        $scope.pairsList.push({ name, chars: [open, close] })
+				$scope.pairsLookUp = {...pairsLookUp, newOpenChar: newClosingChar}
+				$scope.addPair = false
+      }
+
+      $scope.checkWord() = () => {
+				const word = $scope.wordToCheck
+
+				if (word === '') return
+
+				const openingChars = []
+
+				for(const char of word) {
+					if (char in $scope.pairsLookUp.open) {
+						openingChars.push(char)
+					}
+					else if (char in $scope.pairsLookUp.close) {
+						const lastOpenChar = openingChars.pop()
+
+						// word is not balanced
+						if (char !== $scope.pairsLookUp.open[lastOpenChar]) {
+							$scope.checkedWordsHistory.unshift({word, balanced: false})
+						}
+					}
+				}
+
+				// word is balanced
+				$scope.checkedWordsHistory.unshift({word, balanced: true})
+			}
     }
   ])
+
   .directive('balancedBracesTest', () => {
     return {
       templateUrl: 'balanced-braces.tmpl.html'
     }
   })
+
+	.directive('ngEnter', () => {
+    return (scope, element, attrs) => {
+        element.bind("keydown keypress", (event) => {
+            if (event.which === 13) {
+                scope.$apply(() => {
+                    scope.$eval(attrs.ngEnter)
+                })
+                event.preventDefault()
+            }
+        })
+    }
+	})
